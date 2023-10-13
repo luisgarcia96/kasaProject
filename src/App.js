28574c895c86e21318900f568506b611411208e2
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-13 15:53:53 +0200

    Place layouts files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "../src/styles/main.scss";
 import AppRouter from "./router";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<AppRouter />
+			<Footer />
 		</div>
 	);
 }
