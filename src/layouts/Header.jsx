00766b972851e0a71d742fe946c0fdb3f7f5bb9f
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-13 16:50:42 +0200

    Add header basic stylying

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,18 +1,37 @@
 import styles from "../styles/layouts/Header.module.scss";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/logos/logo-normal.svg";
 
 const Header = () => {
+	const location = useLocation();
+
 	return (
 		<header className={styles.header}>
-			<div>{/* Your header content */}</div>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
+			<div className={styles.logoContainer}>
+				<Link to="/">
+					<img src={logo} alt="logo" className={styles.logo} />
+				</Link>
+			</div>
+			<nav className={styles.navContainer}>
+				<ul className={styles.navLinkList}>
+					<li className={styles.navLink}>
+						<Link
+							to="/"
+							className={location.pathname === "/" ? styles.activeLink : ""}
+						>
+							Accueil
+						</Link>
 					</li>
-					<li>
-						<Link to="/about">About us</Link>
+					<li className={styles.navLink}>
+						<Link
+							to="/about"
+							className={
+								location.pathname === "/about" ? styles.activeLink : ""
+							}
+						>
+							A Propos
+						</Link>
 					</li>
 				</ul>
 			</nav>
