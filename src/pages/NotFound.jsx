77eb22dea213bbca1222d59009b061d6fa08fba0
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-13 18:40:03 +0200

    Create 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,19 @@
-import styles from '../styles/pages/NotFound.module.scss'
-import React from 'react';
+import styles from "../styles/pages/NotFound.module.scss";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div className={styles.notFound}>
-            404 page
-        </div>
-    );
+	return (
+		<div className={styles.notFound}>
+			<p className={styles.number}>404</p>
+			<p className={styles.message}>
+				Oups! La page que vous demandez n'existe pas.
+			</p>
+			<Link className={styles.goBack} to="/">
+				Retourner sur la page d'accueil
+			</Link>
+		</div>
+	);
 };
 
 export default NotFound;
