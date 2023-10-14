ea2bb42bfa3f8edeb08cf1275fdd7579b08fe109
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-14 11:54:02 +0200

    Change fonr family

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import styles from '../styles/pages/Home.module.scss'
-import React from 'react';
+import styles from "../styles/pages/Home.module.scss";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div className={styles.home}>
-            This is the Home
-        </div>
-    );
+	return (
+		<div className={styles.home}>
+			<div className={styles.titleContainer}>
+				<h1>Chez vous, partout et ailleurs</h1>
+			</div>
+			<div className={styles.mainContainer}></div>
+		</div>
+	);
 };
 
 export default Home;
