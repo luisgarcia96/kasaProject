367aa06066b6ba36bb563597db47668bd507eaa6
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-15 23:39:25 +0200

    Add Home mobile version

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import styles from '../styles/components/Card.module.scss';
+import styles from "../styles/components/HomeCard.module.scss";
 
-const HomeCard = ({ title , onClick }) => {
-    return (
-        <div className={styles.card} onClick={onClick}>
-            <div className={styles.cardBody}>
-                <h2>{title}</h2>
-            </div>
-        </div>
-    );
+const HomeCard = ({ title, onClick }) => {
+	return (
+		<div className={styles.card} onClick={onClick}>
+			<div className={styles.cardBody}>
+				<h2>{title}</h2>
+			</div>
+		</div>
+	);
 };
 
 HomeCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
+	title: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired,
 };
 
 export default HomeCard;
