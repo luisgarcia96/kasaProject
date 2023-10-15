5b24cfd1b5ae230892acf42769670f610fb25774
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-10-15 20:22:05 +0200

    Finish desktop version

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Accordion from "../components/Accordion";
+import { aboutSections } from "../mocks/aboutPageMock";
+
 import styles from "../styles/pages/About.module.scss";
 
 const About = () => {
-	return <main className={styles.about}>About Page</main>;
+	return (
+		<main className={styles.about}>
+			<div className={styles.titleContainer}></div>
+			<div className={styles.infoContainer}>
+				{aboutSections.map((section, index) => (
+					<Accordion
+						key={`section-${section.title}-${index}`}
+						title={section.title}
+						text={section.content}
+					/>
+				))}
+			</div>
+		</main>
+	);
 };
 
 export default About;
